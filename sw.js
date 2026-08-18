3d39c3e2aefa6161175fc7e6f572a89c34563430
TheHeeHa <kaiparhi0103@gmail.com>
2026-08-18 00:51:30 -0400

    Add advanced enemies and fix broadcast replay

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='verdant-guard-v35';
-const CORE=['./','./index.html','./styles.css?v=20260818-3','./expansion.css?v=20260818-3','./game.js?v=20260818-3','./expansion.js?v=20260818-3','./manifest.webmanifest','./icon-192.png','./icon-512.png','./apple-touch-icon.png','./gameplay.png'];
+const CACHE='verdant-guard-v36';
+const CORE=['./','./index.html','./styles.css?v=20260818-4','./expansion.css?v=20260818-4','./game.js?v=20260818-4','./expansion.js?v=20260818-4','./manifest.webmanifest','./icon-192.png','./icon-512.png','./apple-touch-icon.png','./gameplay.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html'))));});
